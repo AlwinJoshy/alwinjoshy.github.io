de4600b1430373b8367a352142556fc96bd2e5d6
Alwin Joshy <flamencoresonance@gmail.com>
2024-02-08 11:37:12 +0530

    more path updates

--- scripts/loadblogsfromfolder.js
+++ scripts/loadblogsfromfolder.js
@@ -137,8 +137,8 @@ function LoadAllBlogData(allblogs) {
 
 function AutoLoadBlogs() {
 
-    const blogYearTextFilePath = "../components/blogYearSection.html";
-    const blogBlockTextFilePath = "../components/blogContentDisplay.html";
+    const blogYearTextFilePath = "components/blogYearSection.html";
+    const blogBlockTextFilePath = "components/blogContentDisplay.html";
     const blogContentMapFilePath = 'jsonFiles/blogList.json';
 
     LoadTextFile(blogYearTextFilePath,
